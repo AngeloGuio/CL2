b4c2e15ab1865ebb2f75c3357760cdb7dd69be64
AngeloGuio <guioangelo.garcia@gamail.com>
2023-10-29 14:56:52 -0500

    frist commit

---
+++ src/main/java/DAW1_CL2_ANGELO_GARCIA/Producto/ProductoApplication.java
@@ -0,0 +1,13 @@
+package DAW1_CL2_ANGELO_GARCIA.Producto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DAW1_CL2_ANGELO_GARCIA/Producto/ProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package DAW1_CL2_ANGELO_GARCIA.Producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
